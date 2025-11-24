c65f213cb67987795c1487e50187291b5dbf20d9
murik <dennyisgodxd@gmail.com>
2025-11-24 11:45:01 +0100

    Added Controller

--- src/main/java/dk/easv/mytunes/GUI/Main.java
+++ src/main/java/dk/easv/mytunes/GUI/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/views/hello-view.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/views/MainView.fxml"));
         primaryStage.setTitle("MRS");
         primaryStage.setScene(new Scene(root, 320, 240));
         primaryStage.show();
