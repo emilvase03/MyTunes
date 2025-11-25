dca9809bf211af1af79effb754fccc68b06ea5a8
Jakob Primdal <jakobprimdal@gmail.com>
2025-11-25 10:09:38 +0100

    Implemented UserDAO, IUserDataAccess && User classes

---
+++ src/main/java/dk/easv/mytunes/BE/User.java
@@ -0,0 +1,41 @@
+package dk.easv.mytunes.BE;
+
+public class User {
+
+    private int id = -1;
+    private String username = "";
+    private String passwordHash = "";
+
+    public User (int id, String username, String password_hash){
+        setId(id);
+        setUsername(username);
+        setPassword_hash(password_hash);
+    }
+
+    private int getId() {
+        return id;
+    }
+
+    private void setId(int id) {
+        if (id != -1)
+            this.id = id;
+    }
+
+    private String getUsername() {
+        return username;
+    }
+
+    private void setUsername(String username) {
+        if (username != null && !username.isBlank())
+            this.username = username;
+    }
+
+    private String getPassword_hash() {
+        return password_hash;
+    }
+
+    private void setPassword_hash(String password_hash) {
+        if (password_hash != null && !password_hash.isBlank())
+            this.password_hash = password_hash;
+    }
+}
