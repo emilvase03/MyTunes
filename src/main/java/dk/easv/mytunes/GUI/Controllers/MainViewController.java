40f7c0d40f60ca657fdeba0e29561c80d68b3dbb
murik <dennyisgodxd@gmail.com>
2025-11-26 12:07:44 +0100

    added playlists to playlist table

--- src/main/java/dk/easv/mytunes/GUI/Controllers/MainViewController.java
+++ src/main/java/dk/easv/mytunes/GUI/Controllers/MainViewController.java
@@ -1,11 +1,15 @@
 package dk.easv.mytunes.GUI.Controllers;
 
 // Project imports
+import dk.easv.mytunes.BE.Playlist;
 import dk.easv.mytunes.BE.Song;
 import dk.easv.mytunes.GUI.Models.MainViewModel;
+import dk.easv.mytunes.GUI.Models.PlaylistModel;
 
 // Java imports
 import javafx.application.Platform;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -16,16 +20,17 @@ import javafx.scene.Parent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class MainViewController implements Initializable {
 
     private MainViewModel mainViewModel;
+    private PlaylistModel playlistModel;
     private String currentUser;
 
+    // songs
     @FXML
     private TableView<Song> songList;
     @FXML
@@ -37,14 +42,37 @@ public class MainViewController implements Initializable {
     @FXML
     private TableColumn colTime;
 
+    // playlists
+    @FXML
+    private TableView<Playlist> playlistView;
+    @FXML
+    private TableColumn colName;
+    @FXML
+    private TableColumn colSongs;
+    @FXML
+    private TableColumn colPlaylistTime;
+
     @Override
     public void initialize(java.net.URL location, java.util.ResourceBundle resources) {
+
+        // songs table setup
         colTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
         colArtist.setCellValueFactory(new PropertyValueFactory<>("artist"));
         colGenre.setCellValueFactory(new PropertyValueFactory<>("genre"));
         colTime.setCellValueFactory(new PropertyValueFactory<>("Duration"));
 
         songList.setItems(mainViewModel.getObservableSongs());
+
+        // playlist table setup
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colSongs.setCellValueFactory((Callback<TableColumn.CellDataFeatures<Playlist, Integer>, ObservableValue<Integer>>) cellData -> {
+            Playlist playlist = cellData.getValue();
+            int songCount = playlistModel.getSongCount(playlist);
+            return new SimpleIntegerProperty(songCount).asObject();
+        });
+        colPlaylistTime.setCellValueFactory(new PropertyValueFactory<>("duration"));
+
+        playlistView.setItems(playlistModel.getObservablePlaylists());
       
         // Show register page when main view initializes
         Platform.runLater(() -> {
@@ -60,6 +88,7 @@ public class MainViewController implements Initializable {
     public MainViewController() {
         try {
             mainViewModel = new MainViewModel();
+            playlistModel = new PlaylistModel();
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Could not instantiate MainViewModel");
