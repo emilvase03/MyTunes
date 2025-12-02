a580668edf0adfa7e93e19dfc5a789fc1e398edd
murik <dennyisgodxd@gmail.com>
2025-12-02 10:27:44 +0100

    Save local changes before pull

--- src/main/java/dk/easv/mytunes/GUI/Controllers/MainViewController.java
+++ src/main/java/dk/easv/mytunes/GUI/Controllers/MainViewController.java
@@ -5,83 +5,64 @@ import dk.easv.mytunes.BE.Playlist;
 import dk.easv.mytunes.BE.Song;
 import dk.easv.mytunes.GUI.Models.PlaylistModel;
 import dk.easv.mytunes.GUI.Models.SongModel;
+import dk.easv.mytunes.GUI.UTIL.PlaybackManager;
 
 // Java imports
 import javafx.application.Platform;
 import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.util.Callback;
-
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class MainViewController implements Initializable {
 
     private PlaylistModel playlistModel;
     private SongModel songModel;
 
+    private final PlaybackManager playbackManager = new PlaybackManager();
+
     // songs
     @FXML
     private TableView<Song> songList;
     @FXML
-    private TableColumn colTitle;
+    private TableColumn<Song, String> colTitle;
     @FXML
-    private TableColumn colArtist;
+    private TableColumn<Song, String> colArtist;
     @FXML
-    private TableColumn colGenre;
+    private TableColumn<Song, String> colGenre;
     @FXML
-    private TableColumn colTime;
+    private TableColumn<Song, String> colTime;
 
     // playlists
     @FXML
     private TableView<Playlist> playlistView;
     @FXML
-    private TableColumn colName;
+    private TableColumn<Playlist, String> colName;
     @FXML
-    private TableColumn colSongs;
+    private TableColumn<Playlist, Integer> colSongs;
     @FXML
-    private TableColumn colPlaylistTime;
+    private TableColumn<Playlist, String> colPlaylistTime;
 
-    @Override
-    public void initialize(java.net.URL location, java.util.ResourceBundle resources) {
+    // playing songs
+    @FXML
+    private Button playPauseButton;
+    @FXML
+    private Slider volumeBar;
 
-        // songs table setup
-        colTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
-        colArtist.setCellValueFactory(new PropertyValueFactory<>("artist"));
-        colGenre.setCellValueFactory(new PropertyValueFactory<>("genre"));
-        colTime.setCellValueFactory(new PropertyValueFactory<>("Duration"));
 
-        // playlist table setup
-        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
-        colSongs.setCellValueFactory((Callback<TableColumn.CellDataFeatures<Playlist, Integer>, ObservableValue<Integer>>) cellData -> {
-            Playlist playlist = cellData.getValue();
-            int songCount = playlistModel.getSongCount(playlist);
-            return new SimpleIntegerProperty(songCount).asObject();
-        });
-        colPlaylistTime.setCellValueFactory(new PropertyValueFactory<>("duration"));
 
-        playlistView.setItems(playlistModel.getObservablePlaylists());
-        songList.setItems(songModel.getObservableSongs());
-      
-        Platform.runLater(() -> {
-            try {
-                showRegisterPage();
-            } catch (IOException e) {
-                e.printStackTrace();
-                Platform.exit();
-            }
-        });
-    }
-  
     public MainViewController() {
         try {
             playlistModel = new PlaylistModel();
@@ -92,13 +73,71 @@ public class MainViewController implements Initializable {
         }
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        setupTables();
+        bindVolumeSlider();
+
+        playbackManager.playingProperty().addListener((obs, wasPlaying, isPlaying) ->
+                Platform.runLater(() -> playPauseButton.setText(isPlaying ? "Pause" : "Play"))
+        );
+        playPauseButton.setText("Play");
+
+        songList.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 2) {
+                Song song = getSelectedSong();
+                if (song != null) {
+                    playbackManager.playSong(song);
+                }
+            }
+        });
+
+        Platform.runLater(() -> {
+            try {
+                showRegisterPage();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Platform.exit();
+            }
+        });
+    }
+
+    private void setupTables() {
+        // songs table
+        colTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+        colArtist.setCellValueFactory(new PropertyValueFactory<>("artist"));
+        colGenre.setCellValueFactory(new PropertyValueFactory<>("genre"));
+        colTime.setCellValueFactory(new PropertyValueFactory<>("Duration"));
+
+        // playlists table
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colSongs.setCellValueFactory(cellData -> {
+            Playlist playlist = cellData.getValue();
+            int songCount = playlistModel.getSongCount(playlist);
+            return new SimpleIntegerProperty(songCount).asObject();
+        });
+        colPlaylistTime.setCellValueFactory(new PropertyValueFactory<>("duration"));
+
+        playlistView.setItems(playlistModel.getObservablePlaylists());
+        songList.setItems(songModel.getObservableSongs());
+    }
+
+    private void bindVolumeSlider() {
+        volumeBar.setMin(0.0);
+        volumeBar.setMax(1.0);
+        volumeBar.setValue(playbackManager.getVolume());
+
+        volumeBar.valueProperty().addListener((obs, oldVal, newVal) ->
+                playbackManager.setVolume(newVal.doubleValue())
+        );
+    }
+
     private void showRegisterPage() throws IOException {
-        // Load the register view
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/RegisterView.fxml"));
         Parent root = loader.load();
         RegisterViewController registerController = loader.getController();
 
-        // Create and show register stage
         Stage registerStage = new Stage();
         registerStage.setTitle("Welcome to MyTunes");
         registerStage.initModality(Modality.APPLICATION_MODAL);
@@ -106,15 +145,21 @@ public class MainViewController implements Initializable {
         registerStage.setResizable(false);
         registerStage.showAndWait();
 
-        // Check if login was successful
         if (!registerController.isLoginSuccess()) {
-            // User cancelled, close the application
             Platform.exit();
         }
     }
 
     @FXML
-    private void onBtnClickPlayPause() { }
+    private void onBtnClickPlayPause() {
+        if (playlistSelected()) {
+            // playbackManager.playPlaylist(getSelectedPlaylist());
+        } else if (songSelected()) {
+            playbackManager.playSong(getSelectedSong());
+        } else {
+            playbackManager.togglePause();
+        }
+    }
 
     @FXML
     private void onBtnClickPreviousSong() { }
@@ -153,19 +198,14 @@ public class MainViewController implements Initializable {
             stage.setScene(scene);
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.setResizable(false);
-
-            //CSS
-            //scene.getStylesheets().add(getClass().getResource("FilePath").toExternalForm());
-
             stage.showAndWait();
 
             songList.setItems(songModel.getObservableSongs());
 
-            // Auto-select/-scroll to the newly added movie
             NewSongController controller = fxmlLoader.getController();
             if (controller.isSongAdded()) {
                 try {
-                    int newIndex = SongModel.getInstance().getObservableSongs().size() -1;
+                    int newIndex = SongModel.getInstance().getObservableSongs().size() - 1;
                     if (newIndex >= 0) {
                         songList.getSelectionModel().select(newIndex);
                         songList.scrollTo(newIndex);
@@ -174,8 +214,6 @@ public class MainViewController implements Initializable {
                     throw new RuntimeException(e);
                 }
             }
-
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -194,5 +232,21 @@ public class MainViewController implements Initializable {
 
     @FXML
     private void onBtnClickSearch() { }
-}
 
+    // Helper methods
+    private boolean songSelected() {
+        return songList.getSelectionModel().getSelectedItem() != null;
+    }
+
+    private boolean playlistSelected() {
+        return playlistView.getSelectionModel().getSelectedItem() != null;
+    }
+
+    private Song getSelectedSong() {
+        return songList.getSelectionModel().getSelectedItem();
+    }
+
+    private Playlist getSelectedPlaylist() {
+        return playlistView.getSelectionModel().getSelectedItem();
+    }
+}
