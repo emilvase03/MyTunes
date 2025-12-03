057c38d692fe5fa2f2579039438ddaa4b86bf163
Jakob Primdal <jakobprimdal@gmail.com>
2025-12-03 11:21:14 +0100

    Import updated in MainViewController

--- src/main/java/dk/easv/mytunes/GUI/Controllers/MainViewController.java
+++ src/main/java/dk/easv/mytunes/GUI/Controllers/MainViewController.java
@@ -15,6 +15,7 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
